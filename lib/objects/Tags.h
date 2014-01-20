748f14b66c65d73b508300efed4ea74ec2b9f877
Igor Glotov <glotov@jelastic.com>
2014-01-20 22:12:32 +0700

    Response

---
+++ lib/objects/Tags.h
@@ -0,0 +1,26 @@
+//
+// Created by Igor Glotov on 19/01/14.
+// Copyright (c) 2014 undefware. All rights reserved.
+//
+
+
+#ifndef __Tags_H_
+#define __Tags_H_
+
+
+#define TAG_REQUEST_WRAPPER 1000u
+#define TAG_RESPONSE_WRAPPER 2000u
+#define TAG_REQUEST_GET 1100
+#define TAG_RESPONSE_GET_OK 2100u
+#define TAG_RESPONSE_GET_NOT_FOUND 2101u
+#define TAG_RESPONSE_GET_ERROR 2102u
+
+#define TAG_REQUEST_ADD 1200u
+#define TAG_RESPONSE_ADD_OK 2200u
+#define TAG_RESPONSE_ADD_NEED_COMPARE 2201u
+#define TAG_RESPONSE_ADD_ERROR 2202u
+
+#define TAG_REQUEST_ADD_CONTINUE 3100u
+
+
+#endif //__Tags_H_
