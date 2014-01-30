c5d3074fc61766db0307ef3f64789ded59169ef6
Stanislav Ovsyannikov <naphaso@gmail.com>
2014-01-30 22:28:53 +0700

    insert ope test

--- plugin/test.cpp
+++ plugin/test.cpp
@@ -4,10 +4,78 @@
 #include "Request.h"
 #include "ObjectParser.h"
 
+void *context;
+void *socket;
+
+class InsertListener : public ObjectListener {
+
+public:
+    virtual void OnRequestGet(unsigned int requestId, RequestGet *request) {
+        printf("request get\n");
+    }
+
+    virtual void OnResponseGetEmpty(unsigned int requestId) {
+        printf("response get empty\n");
+    }
+
+    virtual void OnResponseGetOk(unsigned int requestId, ResponseGetOk *response) {
+        printf("response get ok\n");
+    }
+
+    virtual void OnRequestAdd(unsigned int requestId, RequestAdd *request) {
+        printf("request add\n");
+    }
+
+
+    virtual void OnResponseAddOk(unsigned int requestId) {
+        printf("response add ok: %u\n", requestId);
+    }
+
+    virtual void OnResponseAddTraverse(unsigned int requestId, ResponseAddTraverse *response) {
+        printf("response add traverse, request id %u init request id %u, ciphertext size %u\n", requestId, response->initRequestId(), response->ciphertext()->size());
+
+        RequestAddContinue request;
+        request.initRequestId() = response->initRequestId();
+        request.compareResult() = 1;
+
+        RequestWrapper wrapper;
+        wrapper.setId((unsigned int) rand());
+        wrapper.setRequest(&request);
+
+        CborOutput output(10000);
+        CborWriter writer(output);
+        wrapper.Serialize(writer);
+
+        zmq_send(socket, output.getData(), (size_t) output.getSize(), 0);
+
+        zmq_msg_t message;
+        zmq_msg_init(&message);
+        zmq_msg_recv(&message, socket, 0);
+
+        CborInput input(zmq_msg_data(&message), zmq_msg_size(&message));
+        ObjectParser parser;
+        parser.SetInput(input);
+        parser.SetListener(*this);
+        parser.Run();
+
+        delete response;
+    }
+
+
+    virtual void OnRequestAddContinue(unsigned int requestId, RequestAddContinue *request) {
+        printf("request add continue\n");
+    }
+
+    virtual void OnError(const char *error) {
+        printf("error: %s\n", error);
+    }
+};
+
 int main(int argc, char **argv) {
 
-	void *context = zmq_ctx_new();
-	void *socket = zmq_socket(context, ZMQ_REQ);
+	context = zmq_ctx_new();
+	socket = zmq_socket(context, ZMQ_REQ);
+
 	zmq_connect(socket, "tcp://127.0.0.1:9990");
 
     /*
@@ -42,15 +110,11 @@ int main(int argc, char **argv) {
     zmq_msg_init(&message);
     zmq_msg_recv(&message, socket, 0);
 
-    //fwrite(zmq_msg_data(&message), 1, zmq_msg_size(&message), stdout);
-
-
     CborInput input(zmq_msg_data(&message), zmq_msg_size(&message));
     ObjectParser parser;
-    DebugObjectListener listener;
+    InsertListener listener;
     parser.SetInput(input);
     parser.SetListener(listener);
-
     parser.Run();
 
     /*
